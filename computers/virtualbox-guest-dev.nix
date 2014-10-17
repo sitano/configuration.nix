0c28c19fa91c46234d360077c2db5ec3e5c5cf2e
John.Koepi <john.koepi@gmail.com>
2014-10-17 15:22:52 +0300

    vbox seed cmds

--- computers/virtualbox-guest-dev.nix
+++ computers/virtualbox-guest-dev.nix
@@ -1,5 +1,15 @@
 { config, pkgs, ... }:
 
+# $ fdisk /dev/sda # (or whatever device you want to install on)
+# $ mkfs.ext4 -L nixos /dev/sda1
+# $ mkswap -L swap /dev/sda2
+# $ swapon /dev/sda2
+# $ mount /dev/disk/by-label/nixos /mnt
+# $ nixos-generate-config --root /mnt
+# $ nano /mnt/etc/nixos/configuration.nix
+# $ nixos-install
+# $ reboot
+
 {
   imports = [
     ../modules/boot.nix
